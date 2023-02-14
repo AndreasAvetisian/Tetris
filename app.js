384f8c37a4082cff24af336deb7f180b320efb84
Andreas Avetisian <t0avan00@students.oamk.fi>
2023-02-14 16:18:21 +0100

    features
    added score, level, logo, instructions;
    added HittingTheWall function

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ let gameBoardArrayHeight = 20;
 let gameBoardArrayWidth =12;
 let startX = 4;
 let startY = 0;
+let score = 0;
+let level = 1;
+let winOrLose = "Playing";
+let tetrisLogo;
 let coordinateArray = [...Array(gameBoardArrayHeight)].map(e => Array(gameBoardArrayWidth).fill(0));
 let curTetromino = [[1,0], [0,1], [1,1], [2,1]];
 
@@ -13,6 +17,7 @@ let tetrominoColor = ['purple', 'cyan', 'blue', 'yellow', 'orange', 'green', 're
 let curTetrominoColor;
 
 let gameBoardArray = [...Array(gameBoardArrayHeight)].map(e => Array(gameBoardArrayWidth).fill(0));
+let stoppedShapeArray = [...Array(gameBoardArrayHeight)].map(e => Array(gameBoardArrayWidth).fill(0));
 
 let DIRECTION = {
    IDLE: 0,
@@ -49,7 +54,7 @@ function SetupCanvas() {
     canvas.width = 936;
     canvas.height = 956;
 
-    ctx.scale(2,2);
+    //ctx.scale(2,2);
 
     ctx.fillStyle = 'white';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -57,12 +62,45 @@ function SetupCanvas() {
     ctx.strokeStyle = 'black';
     ctx.strokeRect(8, 8, 280, 462);
 
+    tetrisLogo = new Image(161, 54);
+    tetrisLogo.onload = DrawTetrisLogo;
+    tetrisLogo.src = "tetrislogo.png";
+
+    ctx.fillStyle = 'black';
+    ctx.font = '21px Arial';
+    ctx.fillText('SCORE', 300, 98);
+
+    ctx.strokeRect(300, 107, 161, 24);
+
+    ctx.fillText(score.toString(), 310, 127);
+
+    ctx.fillText('LEVEL', 300, 157);
+    ctx.strokeRect(300, 171, 161, 24);
+    ctx.fillText(level.toString(), 310, 190);
+
+    ctx.fillText('WIN / LOSE', 300, 221);
+    ctx.fillText(winOrLose, 310, 261);
+    ctx.strokeRect(300, 232, 161, 95);
+
+    ctx.fillText('CONTROLS', 300, 354);
+    ctx.strokeRect(300, 366, 161, 104);
+    ctx.font = '19px Arial';
+    ctx.fillText('A: Move Left', 310, 388);
+    ctx.fillText('D: Move Right', 310, 413);
+    ctx.fillText('S: Move Down', 310, 438);
+    ctx.fillText('E: Rotate Right', 310, 463);
+
+
     document.addEventListener('keydown', HandleKeyPress);
     CreateTetrominos();
     CreateTetromino();
 
     CreateCoordArray();
     DrawTetromino();
+}
+
+function DrawTetrisLogo() {
+    ctx.drawImage(tetrisLogo, 300, 8, 161, 54);
 }
 
 function DrawTetromino() {
@@ -80,19 +118,25 @@ function DrawTetromino() {
 function HandleKeyPress(key) {
     if (key.keyCode === 65) {
         direction = DIRECTION.LEFT;
-        DeleteTetromino();
-        startX--;
-        DrawTetromino();
+        if (!HittingTheWall()) {
+            DeleteTetromino();
+            startX--;
+            DrawTetromino();
+        }
     } else if (key.keyCode === 68) {
         direction = DIRECTION.RIGHT;
-        DeleteTetromino();
-        startX++;
-        DrawTetromino();
+        if (!HittingTheWall()) {
+            DeleteTetromino();
+            startX++;
+            DrawTetromino();
+        }
     } else if (key.keyCode === 83) {
         direction = DIRECTION.DOWN;
-        DeleteTetromino();
-        startY++;
-        DrawTetromino();
+        if (!HittingTheWall()) {
+            DeleteTetromino();
+            startY++;
+            DrawTetromino();
+        }
     }
 }
 
@@ -129,4 +173,16 @@ function CreateTetromino() {
     let randomTetromino = Math.floor(Math.random() * tetrominos.length);
     curTetromino = tetrominos[randomTetromino];
     curTetrominoColor = tetrominoColor[randomTetromino];
+}
+
+function HittingTheWall() {
+    for (let i = 0; i < curTetromino.length; i++) {
+        let newX = curTetromino[i][0] + startX;
+        if (newX <= 0 && direction === DIRECTION.LEFT) {
+            return true;
+        } else if (newX >= 11 && direction === DIRECTION.RIGHT) {
+            return true;
+        }
+    }
+    return false;
 }
